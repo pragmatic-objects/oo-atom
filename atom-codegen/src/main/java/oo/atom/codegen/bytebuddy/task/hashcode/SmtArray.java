b6bb7aa158325901458deadf7342f3e592f76de4
Kapralov Sergey <skapralov@mail.ru>
2017-07-19 07:44:12 +0300

    Improved modular structure of the project. Introduced atom-starter

---
+++ atom-codegen/src/main/java/oo/atom/codegen/bytebuddy/task/hashcode/SmtArray.java
@@ -0,0 +1,61 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.codegen.bytebuddy.task.hashcode;
+
+import javaslang.collection.List;
+import javaslang.control.Try;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bytecode.StackManipulation;
+import net.bytebuddy.implementation.bytecode.collection.ArrayFactory;
+import oo.atom.task.Task;
+
+/**
+ *
+ * @author Kapralov Sergey
+ */
+public class SmtArray implements Task<StackManipulation> {
+
+    private final List<Task<StackManipulation>> members;
+
+    public SmtArray(List<Task<StackManipulation>> members) {
+        this.members = members;
+    }
+
+    public SmtArray(Task<StackManipulation>... members) {
+        this(List.of(members));
+    }
+
+    @Override
+    public final Try<StackManipulation> result() {
+        Try<StackManipulation> result = members
+                .map(Task::result)
+                .transform(Try::sequence)
+                .map(seq -> ArrayFactory.forType(TypeDescription.Generic.OBJECT).withValues(
+                            seq.toJavaList()
+                    )
+                );
+        
+        return result;
+    }
+}
---
+++ atom-codegen/src/main/java/oo/atom/task/TChain.java
@@ -0,0 +1,53 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.task;
+
+import javaslang.collection.List;
+import javaslang.control.Try;
+
+/**
+ *
+ * @author Kapralov Sergey
+ */
+public class TChain<V> implements Task<V> {
+    private final Try<V> value;
+    private final List<TaskLink<V>> links;
+
+    public TChain(Try<V> value, List<TaskLink<V>> links) {
+        this.value = value;
+        this.links = links;
+    }
+    
+    public TChain(Try<V> value, TaskLink<V>... links) {
+        this(value, List.of(links));
+    }
+
+    @Override
+    public final Try<V> result() {
+        return links.foldLeft(value, (smtr, smtl) -> {
+            Try<Task<V>> taskOpt = smtr.map(smtl::task);
+            return taskOpt.map(Task::result).getOrElse(smtr);
+        });
+    }
+}
