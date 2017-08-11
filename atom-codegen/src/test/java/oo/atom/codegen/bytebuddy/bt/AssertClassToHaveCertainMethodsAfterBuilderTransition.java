b7c747ad322d77bbf0c31ffa792d9818f694f62d
Kapralov Sergey <skapralov@mail.ru>
2017-08-11 23:26:18 +0300

    Changed byte-buddy transformation from REBASE to REDEFINE.

---
+++ atom-codegen/src/test/java/oo/atom/codegen/bytebuddy/bt/AssertClassToHaveCertainMethodsAfterBuilderTransition.java
@@ -0,0 +1,81 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.codegen.bytebuddy.bt;
+
+import io.vavr.collection.List;
+import java.lang.reflect.Method;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType;
+import oo.atom.tests.Assertion;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ *
+ * @author Kapralov Sergey
+ */
+public class AssertClassToHaveCertainMethodsAfterBuilderTransition implements Assertion {
+    private final String description;
+    private final BuilderTransition bt;
+    private final Class<?> clazz;
+    private final List<String> methodNames;
+
+    public AssertClassToHaveCertainMethodsAfterBuilderTransition(String description, BuilderTransition bt, Class<?> clazz, List<String> methodNames) {
+        this.description = description;
+        this.bt = bt;
+        this.clazz = clazz;
+        this.methodNames = methodNames;
+    }
+    
+    public AssertClassToHaveCertainMethodsAfterBuilderTransition(String description, BuilderTransition bt, Class<?> clazz, String... methodNames) {
+        this(
+            description,
+            bt,
+            clazz,
+            List.of(methodNames)
+        );
+    }
+
+    @Override
+    public final String description() {
+        return description;
+    }
+
+    @Override
+    public final void check() throws Exception {
+        final TypeDescription typeDescription = new TypeDescription.ForLoadedType(clazz);
+        final DynamicType.Builder<?> subclass = new ByteBuddy().redefine(clazz);
+        final DynamicType.Unloaded<?> make = bt
+                .transitionResult(subclass, typeDescription)
+                .outcome()
+                .get()
+                .make();
+        final Class<?> newClazz = make.load(new ClassLoader() {}).getLoaded();
+        assertThat(
+            List.of(newClazz.getDeclaredMethods()).map(Method::getName)
+        ).containsOnlyElementsOf(
+            methodNames
+        );
+    }
+}
