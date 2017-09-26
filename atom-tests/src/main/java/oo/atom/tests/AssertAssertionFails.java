895e46774f242a80632409b25d93b6532136cb69
Kapralov Sergey <skapralov@mail.ru>
2017-09-26 17:47:21 +0300

    [#48] Implement assertions for testing another assertions and covered them with tests.

---
+++ atom-tests/src/main/java/oo/atom/tests/AssertAssertionFails.java
@@ -0,0 +1,52 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.tests;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * Assertion which passes when the assertion under the test fails with
+ * {@link AssertionError}
+ * 
+ * @author Kapralov Sergey
+ */
+public class AssertAssertionFails implements Assertion {
+    private final String description;
+    private final Assertion assertion;
+
+    public AssertAssertionFails(String description, Assertion assertion) {
+        this.description = description;
+        this.assertion = assertion;
+    }
+
+    @Override
+    public final String description() {
+        return description;
+    }
+
+    @Override
+    public final void check() throws Exception {
+        assertThatThrownBy(() -> assertion.check()).isInstanceOf(AssertionError.class);
+    }
+}
