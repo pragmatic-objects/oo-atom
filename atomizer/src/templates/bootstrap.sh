d1ce653aca3ce7777605ee056ba5d0e81370b375
Kapralov Sergey <skapralov@mail.ru>
2017-12-23 00:42:13 +0300

    [#112] Fixed classpath of Atomizer
    Added debug output of atomizer cmdline
    Removed aggro mode (to be restored when ready)

--- atomizer/src/templates/bootstrap.sh
+++ atomizer/src/templates/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -
-
 JAVA="java";
 if [ ! -z ${JAVA_HOME} ]; then
     JAVA="${JAVA_HOME}/bin/java";
 fi
 
-exec ${JAVA} -jar $0 $@
+echo "${JAVA} -cp $0:$1 oo.atom.codegen.AtomizerMain"
+exec ${JAVA} -cp "$0:$1" oo.atom.codegen.AtomizerMain
