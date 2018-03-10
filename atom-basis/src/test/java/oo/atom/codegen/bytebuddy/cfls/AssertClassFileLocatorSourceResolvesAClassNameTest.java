fe76269e84273da835bbd7cc4fb3908ec167c5c1
Kapralov Sergey <skapralov@natera.com>
2018-03-10 15:07:17 +0300

    [#8] ClassFileLocatorSource prepared for testing

---
+++ atom-basis/src/test/java/oo/atom/codegen/bytebuddy/cfls/AssertClassFileLocatorSourceResolvesAClassNameTest.java
@@ -0,0 +1,65 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.codegen.bytebuddy.cfls;
+
+import oo.atom.tests.AssertAssertionFails;
+import oo.atom.tests.AssertAssertionPasses;
+import oo.atom.tests.TestCase;
+import oo.atom.tests.TestsSuite;
+
+import static oo.atom.codegen.bytebuddy.cfls.TestData.*;
+
+/**
+ * Tests suite for {@link AssertClassFileLocatorSourceResolvesAClassName}
+ *
+ * @author Kapralov Sergey
+ */
+public class AssertClassFileLocatorSourceResolvesAClassNameTest extends TestsSuite {
+    public AssertClassFileLocatorSourceResolvesAClassNameTest() {
+        super(
+            new TestCase(
+                "positive case",
+                new AssertAssertionPasses(
+                    new AssertClassFileLocatorSourceResolvesAClassName(
+                        new CflsExplicit(
+                            CFL_FOO
+                        ),
+                        FOO_NAME
+                    )
+                )
+            ),
+            new TestCase(
+                "negative case",
+                new AssertAssertionFails(
+                    new AssertClassFileLocatorSourceResolvesAClassName(
+                        new CflsExplicit(
+                            CFL_FOO
+                        ),
+                        BAR_NAME
+                    )
+                )
+            )
+        );
+    }
+}
---
+++ atom-basis/src/test/java/oo/atom/codegen/bytebuddy/cfls/TestData.java
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Kapralov Sergey.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package oo.atom.codegen.bytebuddy.cfls;
+
+import net.bytebuddy.dynamic.ClassFileLocator;
+import oo.atom.anno.NotAtom;
+import org.apache.commons.io.IOUtils;
+
+import java.util.HashMap;
+
+@NotAtom
+final class TestData {
+    public static final ClassFileLocator CFL_FOO;
+
+    static {
+        try {
+            final HashMap<String, byte[]> bcMap = new HashMap<>();
+            final String fooName = Foo.class.getName();
+            final String fooPath = fooName.replace(".", "/") + ".class";
+            bcMap.put(fooName, IOUtils.resourceToByteArray(
+                    fooPath,
+                    AssertClassFileLocatorSourceDoesntResolveAClassNameTest.class.getClassLoader()
+            ));
+            CFL_FOO = new ClassFileLocator.Simple(bcMap);
+        } catch(Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    public static final class Foo {}
+    public static final String FOO_NAME = Foo.class.getName();
+    public static final class Bar {}
+    public static final String BAR_NAME = Bar.class.getName();
+}
